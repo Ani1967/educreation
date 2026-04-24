e0227b28a31d9c37fad9ee1864d4ee5d4f6b7738
Aniruddha Kar <aniruddhaamacbookpro@Aniruddhas-MacBook-Pro.local>
2026-04-24 19:11:00 +0530

    Add Google verification meta tag directly to head

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="google-site-verification" content="O44pzGtYeq-OLUIbMgUUUe7W-14msQl1oKnGGAvdgII" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
